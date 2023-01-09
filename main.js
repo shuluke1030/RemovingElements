db2598ad920656dad5ae4bbe4f6262218e1dd2a4
shuluke1030 <shuluke1030@gmail.com>
2023-01-09 18:31:08 +0800

    finish

---
+++ main.js
@@ -0,0 +1,12 @@
+function removeEveryOther(arr){
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        if(i % 2 === 0){
+            result.push(arr[i]);
+        }
+        
+    }
+   
+    return result;
+
+}
